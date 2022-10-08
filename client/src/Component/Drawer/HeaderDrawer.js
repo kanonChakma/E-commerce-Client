048dd3b9d0a2809354376d93cf55a8b6289f1d78
kanon chakma <kanonchakma@pop-os.localdomain>
2022-10-08 13:50:21 +0600

    feat: refactor headerDrawer

--- client/src/Component/Drawer/HeaderDrawer.js
+++ client/src/Component/Drawer/HeaderDrawer.js
@@ -1,41 +1,176 @@
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { Drawer, IconButton } from "@mui/material";
-import { Menu } from 'antd';
-import React, { useState } from 'react';
-const { SubMenu } = Menu;
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MenuIcon from "@mui/icons-material/Menu";
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+const paths=[
+    {
+        icon:<InboxIcon />,
+        text:"About us"
+    },
+    {
+        icon:<InboxIcon />,
+        text:'NDIS'
+    },
+    {
+        icon:<InboxIcon />,
+        text:"Covid 19"
+    },
+    {
+        icon:<InboxIcon />,
+        text:'Serivices'
+    },
+    {
+        icon:<InboxIcon />,
+        text:'Career'
+    },
+]
+const pages=["about","ndis","covid","service","career"];
+const HeaderDrawer = ({user,logout, cart}) => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [anchorEl, setAnchorEl] =  useState(null);
+  const open = Boolean(anchorEl);
 
-
-const HeaderDrawer = () => {
-   const [openDrawer, setOpenDrawer] = useState(false);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
       <Drawer
-      PaperProps={{
-        elevation: 8,
-        sx: {
-          marginTop:15,
-          width: 240,
-          height: 500,
-          color: 'black',
-          backgroundColor: "white"
-        }
-      }}
         anchor="left"
         open={openDrawer}
         onClose={() => setOpenDrawer(false)}
       >
-    
+       <Box   sx={{height:'100vh',width:{sm:'250px',xs:'190px'}}}>
+       <List>
+          {paths.map((page, index) => (
+          <Link href={`/${pages[index]}`}  key={index}>
+            <ListItemButton onClick={() => setOpenDrawer(false)}>
+                <ListItemIcon>
+                    {page.icon}
+                </ListItemIcon>
+                <ListItemText>{page.text}</ListItemText>
+            </ListItemButton>
+          </Link>
+          ))}
+        </List>
+       </Box>
       </Drawer>
-      <IconButton
-        sx={{ color: "black", marginLeft: "auto" }}
-        onClick={() => setOpenDrawer(!openDrawer)}
+      
+      <AppBar position="static" color="transparent">
+      <Toolbar>
+        <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+          <MenuIcon  />
+         </IconButton >
+         
+         <Button color="violet">
+           <Link  style={{ color: '#616161' }}  to="/"> Home </Link>
+         </Button>
+         
+         <Button>
+          <Link  style={{ color: '#616161' }}  to="/shop"> Shop </Link>
+         </Button>
+       
+       <Button>
+         <Link style={{ color: '#616161' }} to="/cart">
+           Cart 
+          <Badge count={cart.length} offset={[0,5]}>
+         </Badge>
+         </Link>
+       </Button>
+
+        <div style={{flexGrow: "1"}}></div>
+
+       <Tooltip title="Account settings">
+           <IconButton
+           onClick={handleClick}
+        >
+        <AccountCircleIcon />
+           </IconButton>
+        </Tooltip>
+        <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <FilterListIcon />
-      </IconButton>
+        {!user && <MenuItem>
+            <Avatar/> <Link to="/login">Login Account</Link>
+        </MenuItem>}
+
+       {!user && <MenuItem>
+        <Avatar /> <Link to='/register'>Register Account</Link>
+        </MenuItem>}
+
+      {user && user.role ==="subscriber" && <MenuItem>
+          <Avatar/> <Link to="/user/history">User Dashboard</Link>
+      </MenuItem>}
+
+      {user && user.role ==="admin" && <MenuItem>
+      <Avatar/> <Link to="/admin/dashboard">Admin Dashboard</Link>
+      </MenuItem>}
+
+      {user && <MenuItem
+        onClick={logout}
+        >
+         <Avatar /> <Link>Logout Account</Link>
+      </MenuItem>}
+      
+      </Menu>
+      </Toolbar>
+    </AppBar>
     </>
   );
 };
 
 export default HeaderDrawer;
-
